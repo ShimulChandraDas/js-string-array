bd7eb7b8a93eafdebb26c39223cdc581f37d0612
ShimulChandraDas <msshimul99@gmail.com>
2022-02-13 20:54:03 +0600

    String comparison using toLowerCase toUpperCase

---
+++ case-converet.js
@@ -0,0 +1,9 @@
+const anthem = " Amar Shomnar bangla Ami tomai vhalobashi";
+const serch = 'vhalo';
+
+const userInput = 'balckPINK';
+const savedUserName = 'balckPink';
+console.log(userInput.toUpperCase());
+// if (userInput.toUpperCase() == savedUserName.toUpperCase()) {
+//     console.log('User Exist');
+// }
